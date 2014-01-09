6ba831c7a392e1c4f4bceec6417be05e3234fd28
Joshua Potgieter <supermanlovespink@gmail.com>
2014-01-09 08:09:15 +0200

    yeah oh baby yeah

--- cms.go
+++ cms.go
@@ -1,1 +1,44 @@
-package cms
+package main
+
+import (
+	"code.google.com/p/gorest"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Article struct {
+	Title   string
+	Date    string
+	Content []string
+}
+
+func main() {
+	fmt.Println("Starting up")
+	fileContent, fileErr := ioutil.ReadFile("test.json")
+	if fileErr != nil {
+		fmt.Println("Error reading file : " + fileErr.Error())
+	}
+	var m Article
+
+	err := json.Unmarshal(fileContent, &m)
+	if err != nil {
+		fmt.Println("Error unmarshalling file contents to JSON : " + err.Error())
+	}
+	fmt.Println(m.Content)
+	gorest.RegisterService(new(ContentService))
+	http.Handle("/", gorest.Handle())
+	http.ListenAndServe(":8787", nil)
+	fmt.Println("Shutting Down")
+}
+
+type ContentService struct {
+	gorest.RestService `root:"/" consumes:"application/json" produces:"application/json"`
+	getArticle         gorest.EndPoint `method:"GET" path:"/articles/{ArticleName:string}" output:"string"`
+}
+
+func (serv ContentService) GetArticle(ArticleName string) (out string) {
+	serv.ResponseBuilder().SetResponseCode(404).Overide(true)
+	return
+}
